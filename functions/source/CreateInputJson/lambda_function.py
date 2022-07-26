58eafb5ba64ecd05051337aecaeeda4130589298
andreidorin-oprea <87006060+andreidorin-oprea@users.noreply.github.com>
2022-07-26 13:36:04 +0300

    Update templates to version 2022.4.1

--- functions/source/CreateInputJson/lambda_function.py
+++ functions/source/CreateInputJson/lambda_function.py
@@ -3,6 +3,8 @@ import json
 import cfnresponse
 import threading
 import uuid
+import random
+import string
 
 
 def create(properties, physical_id):
@@ -12,6 +14,7 @@ def create(properties, physical_id):
     platform_secret_arn = properties['PlatformSecretArn']
     org_secret_arn = properties['OrgSecretArn']
     argocd_secret_arn = properties['ArgoCdSecretArn']
+    argocd_user_secret_arn = properties['ArgoCdUserSecretArn']
     fqdn = properties["Fqdn"]
     db_endpoint = properties["RDSDBInstanceEndpointAddress"]
     multi_node = properties["MultiNode"]
@@ -19,6 +22,7 @@ def create(properties, physical_id):
     action_center = properties['ActionCenter']
     test_manager = properties['TestManager']
     insights = properties['Insights']
+    data_service = properties['DataService']
     automation_hub = properties['AutomationHub']
     automation_ops = properties['AutomationOps']
     task_mining = properties['TaskMining']
@@ -30,15 +34,16 @@ def create(properties, physical_id):
     agent_instance_count = int(properties['AgentInstanceCount'])
     private_subnet_ids = properties['PrivateSubnetIDs']
     extra_dict_keys = properties['ExtraConfigKeys']
+    self_signed_cert_validity = properties['SelfSignedCertificateValidity']
 
     initial_number_of_instances = server_instance_count + agent_instance_count
 
     ret = {"fqdn": fqdn, "rke_token": str(uuid.uuid4())}
+    ret['cloud_template_vendor'] = 'AWS'
+    ret['cloud_template_source'] = 'Quickstart'
 
     ret['fixed_rke_address'] = internal_load_balancer_dns
     if multi_node.lower() == 'multi node':
-        ret['multinode'] = 'true'
-        ret['ha'] = 'true'
         ret['profile'] = 'ha'
         subnet_list = private_subnet_ids.split(',')
         if len(subnet_list) >= 3:
@@ -46,15 +51,10 @@ def create(properties, physical_id):
         else:
             ret['zone_resilience'] = False
     else:
-        ret['multinode'] = 'false'
-        ret['ha'] = 'false'
         ret['profile'] = 'default'
 
     if add_gpu.lower() == 'true':
         initial_number_of_instances += 1
-        ret['gpu_support'] = True
-    else:
-        ret["gpu_support"] = False
 
     sm = boto3.client('secretsmanager', region_name=region)
 
@@ -81,6 +81,14 @@ def create(properties, physical_id):
     print("Adding ArgoCD username and password to JSON")
     ret['fabric'] = {"argocd_admin_password": secret['password']}
 
+    print("Getting ArgoCD readonly User secret")
+    argocd_user_secret = sm.get_secret_value(
+        SecretId=argocd_user_secret_arn
+    )
+    secret = json.loads(argocd_user_secret['SecretString'])
+    print("Adding ArgoCD readonly user's password to JSON")
+    ret['fabric']["argocd_user_password"] = secret['password']
+
     # fix issue with non existing server certificates file
     ret["server_certificate"] = {}
     ret["server_certificate"]["ca_cert_file"] = "/root/rootCA.crt"
@@ -89,8 +97,10 @@ def create(properties, physical_id):
 
     ret["identity_certificate"] = {}
     ret["identity_certificate"]["token_signing_cert_file"] = "/root/token_signing_certificate.pfx"
-    ret["identity_certificate"]["token_signing_cert_pass"] = ""
+    ret["identity_certificate"]["token_signing_cert_pass"] = ''.join(random.choice(string.ascii_letters) for i in range(20))
     ret["identity_certificate"]["ldap_cert_authority_file"] = ""
+
+    ret['self_signed_cert_validity'] = self_signed_cert_validity
 
     if extra_dict_keys:
         try:
@@ -109,17 +119,15 @@ def create(properties, physical_id):
     )
     secret = json.loads(db_secret['SecretString'])
     ret["sql"] = {}
-    ret["sql"]["server_url"] = db_endpoint
-    ret["sql"]["username"] = secret['username']
-    ret["sql"]["password"] = secret['password']
-    ret["sql"]["port"] = '1433'
     ret["sql"]["create_db"] = True
 
-    sql_connection_string = f"Server=tcp:{db_endpoint},1433;Initial Catalog=DB_NAME_PLACEHOLDER;Persist Security Info=False;User Id={secret['username']};Password={secret['password']};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;Max Pool Size=100;"
+    dot_net_escaped_password = secret['password'].replace("'", "''")
+    odbc_escaped_password = secret['password'].replace("}", "}}")
+
     print("Adding SQL connection strings to JSON")
-    ret["sql_connection_string_template"] = sql_connection_string
-    ret["sql_connection_string_template_jdbc"] = f"jdbc:sqlserver://{db_endpoint};database=DB_NAME_PLACEHOLDER;user={secret['username']};password={secret['password']}"
-    ret["sql_connection_string_template_odbc"] = f"SERVER={db_endpoint};DATABASE=DB_NAME_PLACEHOLDER;DRIVER={{ODBC Driver 17 for SQL Server}};UID={secret['username']};PWD={secret['password']}"
+    ret["sql_connection_string_template"] = f"Server=tcp:{db_endpoint},1433;Initial Catalog=DB_NAME_PLACEHOLDER;Persist Security Info=False;User Id={secret['username']};Password='{dot_net_escaped_password}';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;Max Pool Size=100;"
+    ret["sql_connection_string_template_jdbc"] = f"jdbc:sqlserver://{db_endpoint};database=DB_NAME_PLACEHOLDER;user={secret['username']};password={{{odbc_escaped_password}}}"
+    ret["sql_connection_string_template_odbc"] = f"SERVER={db_endpoint};DATABASE=DB_NAME_PLACEHOLDER;DRIVER={{ODBC Driver 17 for SQL Server}};UID={secret['username']};PWD={{{odbc_escaped_password}}}"
 
     ret['orchestrator'] = {}
     ret['orchestrator']['testautomation'] = {"enabled": True}
@@ -139,6 +147,11 @@ def create(properties, physical_id):
         ret['action_center'] = {"enabled": True}
     else:
         ret['action_center'] = {"enabled": False}
+
+    if data_service.lower() == "true":
+        ret['dataservice'] = {"enabled": True}
+    else:
+        ret['dataservice'] = {"enabled": False}
 
     if test_manager.lower() == "true":
         ret['test_manager'] = {"enabled": True}
@@ -199,11 +212,13 @@ def update(properties, physical_id):
     platform_secret_arn = properties['PlatformSecretArn']
     org_secret_arn = properties['OrgSecretArn']
     argocd_secret_arn = properties['ArgoCdSecretArn']
+    argocd_user_secret_arn = properties['ArgoCdUserSecretArn']
     fqdn = properties["Fqdn"]
     db_endpoint = properties["RDSDBInstanceEndpointAddress"]
     multi_node = properties["MultiNode"]
     internal_load_balancer_dns = properties["KubeLoadBalancerDns"]
     action_center = properties['ActionCenter']
+    data_service = properties['DataService']
     test_manager = properties['TestManager']
     insights = properties['Insights']
     automation_hub = properties['AutomationHub']
@@ -217,15 +232,16 @@ def update(properties, physical_id):
     agent_instance_count = int(properties['AgentInstanceCount'])
     private_subnet_ids = properties['PrivateSubnetIDs']
     extra_dict_keys = properties['ExtraConfigKeys']
+    self_signed_cert_validity = properties['SelfSignedCertificateValidity']
 
     initial_number_of_instances = server_instance_count + agent_instance_count
 
     ret = {"fqdn": fqdn, "rke_token": str(uuid.uuid4())}
+    ret['cloud_template_vendor'] = 'AWS'
+    ret['cloud_template_source'] = 'Quickstart'
 
     ret['fixed_rke_address'] = internal_load_balancer_dns
     if multi_node.lower() == 'multi node':
-        ret['multinode'] = 'true'
-        ret['ha'] = 'true'
         ret['profile'] = 'ha'
         subnet_list = private_subnet_ids.split(',')
         if len(subnet_list) >= 3:
@@ -233,15 +249,10 @@ def update(properties, physical_id):
         else:
             ret['zone_resilience'] = False
     else:
-        ret['multinode'] = 'false'
-        ret['ha'] = 'false'
         ret['profile'] = 'default'
 
     if add_gpu.lower() == 'true':
         initial_number_of_instances += 1
-        ret['gpu_support'] = True
-    else:
-        ret["gpu_support"] = False
 
     sm = boto3.client('secretsmanager', region_name=region)
 
@@ -268,6 +279,14 @@ def update(properties, physical_id):
     print("Adding ArgoCD username and password to JSON")
     ret['fabric'] = {"argocd_admin_password": secret['password']}
 
+    print("Getting ArgoCD readonly User secret")
+    argocd_user_secret = sm.get_secret_value(
+        SecretId=argocd_user_secret_arn
+    )
+    secret = json.loads(argocd_user_secret['SecretString'])
+    print("Adding ArgoCD readonly user's password to JSON")
+    ret['fabric']["argocd_user_password"] = secret['password']
+
     # fix issue with non existing server certificates file
     ret["server_certificate"] = {}
     ret["server_certificate"]["ca_cert_file"] = "/root/rootCA.crt"
@@ -276,8 +295,10 @@ def update(properties, physical_id):
 
     ret["identity_certificate"] = {}
     ret["identity_certificate"]["token_signing_cert_file"] = "/root/token_signing_certificate.pfx"
-    ret["identity_certificate"]["token_signing_cert_pass"] = ""
+    ret["identity_certificate"]["token_signing_cert_pass"] = ''.join(random.choice(string.ascii_letters) for i in range(20))
     ret["identity_certificate"]["ldap_cert_authority_file"] = ""
+
+    ret['self_signed_cert_validity'] = self_signed_cert_validity
 
     if extra_dict_keys:
         try:
@@ -296,17 +317,15 @@ def update(properties, physical_id):
     )
     secret = json.loads(db_secret['SecretString'])
     ret["sql"] = {}
-    ret["sql"]["server_url"] = db_endpoint
-    ret["sql"]["username"] = secret['username']
-    ret["sql"]["password"] = secret['password']
-    ret["sql"]["port"] = '1433'
     ret["sql"]["create_db"] = True
 
-    sql_connection_string = f"Server=tcp:{db_endpoint},1433;Initial Catalog=DB_NAME_PLACEHOLDER;Persist Security Info=False;User Id={secret['username']};Password={secret['password']};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;Max Pool Size=100;"
+    dot_net_escaped_password = secret['password'].replace("'", "''")
+    odbc_escaped_password = secret['password'].replace("}", "}}")
+
     print("Adding SQL connection strings to JSON")
-    ret["sql_connection_string_template"] = sql_connection_string
-    ret["sql_connection_string_template_jdbc"] = f"jdbc:sqlserver://{db_endpoint};database=DB_NAME_PLACEHOLDER;user={secret['username']};password={secret['password']}"
-    ret["sql_connection_string_template_odbc"] = f"SERVER={db_endpoint};DATABASE=DB_NAME_PLACEHOLDER;DRIVER={{ODBC Driver 17 for SQL Server}};UID={secret['username']};PWD={secret['password']}"
+    ret["sql_connection_string_template"] = f"Server=tcp:{db_endpoint},1433;Initial Catalog=DB_NAME_PLACEHOLDER;Persist Security Info=False;User Id={secret['username']};Password='{dot_net_escaped_password}';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;Max Pool Size=100;"
+    ret["sql_connection_string_template_jdbc"] = f"jdbc:sqlserver://{db_endpoint};database=DB_NAME_PLACEHOLDER;user={secret['username']};password={{{odbc_escaped_password}}}"
+    ret["sql_connection_string_template_odbc"] = f"SERVER={db_endpoint};DATABASE=DB_NAME_PLACEHOLDER;DRIVER={{ODBC Driver 17 for SQL Server}};UID={secret['username']};PWD={{{odbc_escaped_password}}}"
 
     ret['orchestrator'] = {}
     ret['orchestrator']['testautomation'] = {"enabled": True}
@@ -326,6 +345,11 @@ def update(properties, physical_id):
         ret['action_center'] = {"enabled": True}
     else:
         ret['action_center'] = {"enabled": False}
+
+    if data_service.lower() == "true":
+        ret['dataservice'] = {"enabled": True}
+    else:
+        ret['dataservice'] = {"enabled": False}
 
     if test_manager.lower() == "true":
         ret['test_manager'] = {"enabled": True}
--- scripts/install-server.sh
+++ scripts/install-server.sh
@@ -4,9 +4,13 @@ set -eu
 export PATH=$PATH:/var/lib/rancher/rke2/bin:/root/installer/bin:/usr/local/bin
 export KUBECONFIG="/etc/rancher/rke2/rke2.yaml"
 
+SETUPBACKUP=0
+
 function get_first_server() {
   local instance_id
-  instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+  local metadata_token
+  metadata_token=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")
+  instance_id=$(curl -H "X-aws-ec2-metadata-token: $metadata_token" -s http://169.254.169.254/latest/meta-data/instance-id)
   echo "Local instance id: ${instance_id}"
   asg_name=$(aws autoscaling describe-auto-scaling-instances --instance-ids "${instance_id}" --query 'AutoScalingInstances[*].AutoScalingGroupName' --output text)
   instances=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name "$asg_name" --query 'AutoScalingGroups[*].Instances[?HealthStatus==`Healthy`].InstanceId' --output text)
@@ -14,15 +18,15 @@ function get_first_server() {
   echo "Instances in the autoscaling group: ${instances}"
 
   sorted_instances=$(echo "${instances}" | tr ' ' '\n' | sort | tr '\n' ' ')
-  read -r -a arr_sorted_instances <<< "$sorted_instances"
+  read -r -a arr_sorted_instances <<<"$sorted_instances"
   first_server=$(echo "${instances}" | tr ' ' '\n' | sort | tr '\n' ' ' | awk '{print $1}')
 
   LOCATION_IN_ARRAY=0
   for crt_instance in "${arr_sorted_instances[@]}"; do
     [[ $crt_instance == "$instance_id" ]] && { echo "$LOCATION_IN_ARRAY"; break; }
-    (( ++LOCATION_IN_ARRAY ))
+    ((++LOCATION_IN_ARRAY))
   done
-  LOCATION_IN_ARRAY=$(( LOCATION_IN_ARRAY-1 ))
+  LOCATION_IN_ARRAY=$((LOCATION_IN_ARRAY - 1))
 
   echo "Current instance: ${instance_id} | First server instance: ${first_server} | Current location in array: ${LOCATION_IN_ARRAY}"
 
@@ -35,6 +39,22 @@ function get_first_server() {
   fi
 }
 
+function parse_long_args() {
+  while (("$#")); do
+    case "$1" in
+    -b | --backup)
+      SETUPBACKUP=1
+      shift
+      ;;
+
+    -*) # unsupported flags
+      echo "unsupported flag ${1}"
+      exit 1
+      ;;
+    esac
+  done
+}
+
 function check_registration() {
   local registration_url=$1
   local registration_status
@@ -45,7 +65,7 @@ function check_registration() {
 
   while [[ "${registration_status}" != "200" ]] && ((try != maxtry)); do
     try=$((try + 1))
-    registration_status=$(curl -w '%{response_code}' -sk -o /dev/null https://"${registration_url}":9345/ping) || true
+    registration_status=$(curl -m 5 -w '%{response_code}' -sk -o /dev/null https://"${registration_url}":9345/ping) || true
     echo "Trying to reach ${registration_url} ==== ${try}/${maxtry}" && sleep 2
   done
 
@@ -72,6 +92,9 @@ function main() {
   if [[ "${NODE_TYPE}" == "FIRST_SERVER" ]]; then
     echo "Installing as primary server"
 
+    echo "Generating certificates for first installation"
+    /root/installer/generate-certs.sh
+
     try=0
     maxtry=2
 
@@ -79,7 +102,7 @@ function main() {
     while [[ "${status}" != "ready" ]] && ((try != maxtry)); do
       echo "Trying to install the infra ==== ${try}/${maxtry}"
       try=$((try + 1))
-      /root/installer/install-uipath.sh -i /root/installer/input.json -o /root/installer/output.json -k --accept-license-agreement --skip-pre-reqs && status="ready"
+      /root/installer/install-uipath.sh -i /root/installer/input.json -o /root/installer/output.json -k --accept-license-agreement --skip-pre-reqs --skip-compare-config && status="ready"
     done
 
     [[ "${status}" == "ready" ]] || (echo "Failed to install infra" && exit 1)
@@ -96,13 +119,13 @@ function main() {
     local maxtry=60
     no_of_nodes_actual=$(kubectl get nodes -o jsonpath="$nodereadypath" | grep -c -E "Ready=True")
 
-    while (( no_of_nodes_actual < no_of_nodes_required )) && ((try != maxtry)); do
+    while ((no_of_nodes_actual < no_of_nodes_required)) && ((try != maxtry)); do
       try=$((try + 1))
       echo "Waiting for ${no_of_nodes_required} to be ready, current nodes ${no_of_nodes_actual} ==== ${try}/${maxtry}" && sleep 30
       no_of_nodes_actual=$(kubectl get nodes -o jsonpath="$nodereadypath" | grep -c -E "Ready=True")
     done
 
-    (( no_of_nodes_actual >= no_of_nodes_required )) || (echo "Required number of nodes failed to start" && exit 1)
+    ((no_of_nodes_actual >= no_of_nodes_required)) || (echo "Required number of nodes failed to start" && exit 1)
 
     try=0
     maxtry=2
@@ -111,7 +134,7 @@ function main() {
     while [[ "${status}" != "ready" ]] && ((try != maxtry)); do
       echo "Trying to install the fabric ==== ${try}/${maxtry}"
       try=$((try + 1))
-      /root/installer/install-uipath.sh -i /root/installer/input.json -o /root/installer/output.json -f --accept-license-agreement --skip-pre-reqs && status="ready"
+      /root/installer/install-uipath.sh -i /root/installer/input.json -o /root/installer/output.json -f --accept-license-agreement --skip-pre-reqs --skip-compare-config && status="ready"
     done
 
     [[ "${status}" == "ready" ]] || (echo "Failed to install fabric" && exit 1)
@@ -122,9 +145,22 @@ function main() {
     while [[ "${status}" != "ready" ]] && ((try != maxtry)); do
       echo "Trying to install the services ==== ${try}/${maxtry}"
       try=$((try + 1))
-      /root/installer/install-uipath.sh -i /root/installer/input.json -o /root/installer/output.json -s --accept-license-agreement --skip-pre-reqs && status="ready"
+      /root/installer/install-uipath.sh -i /root/installer/input.json -o /root/installer/output.json -s --accept-license-agreement --skip-pre-reqs --skip-compare-config && status="ready"
     done
     [[ "${status}" == "ready" ]] || (echo "Failed to install services" && exit 1)
+
+    if ((SETUPBACKUP == 1)); then
+      try=0
+      maxtry=2
+
+      status="notready"
+      while [[ "${status}" != "ready" ]] && ((try != maxtry)); do
+        echo "Trying to enable backup ==== ${try}/${maxtry}"
+        try=$((try + 1))
+        /root/installer/install-uipath.sh -i /root/installer/backup.json -o /root/installer/output-backup.json -b --accept-license-agreement && status="ready"
+      done
+      [[ "${status}" == "ready" ]] || (echo "Failed to enable backup" && exit 1)
+    fi
 
   else
     echo "Installing as secondary server"
@@ -133,10 +169,10 @@ function main() {
     local try=0
     local maxtry=60
 
-    registration_status=$(curl -w '%{response_code}' -sk -o /dev/null https://"${registration_url}":9345/ping)  || true
+    registration_status=$(curl -w '%{response_code}' -sk -o /dev/null https://"${registration_url}":9345/ping) || true
     while [[ "${registration_status}" != "200" ]] && ((try != maxtry)); do
       try=$((try + 1))
-      registration_status=$(curl -w '%{response_code}' -sk -o /dev/null https://"${registration_url}":9345/ping)  || true
+      registration_status=$(curl -w '%{response_code}' -sk -o /dev/null https://"${registration_url}":9345/ping) || true
       echo "Trying to reach ${registration_url} ==== ${try}/${maxtry}" && sleep 30
     done
 
@@ -147,7 +183,7 @@ function main() {
     echo "sleeping 7 minutes"
     sleep 420
     # introducing 5 minutes between servers, to avoid the etcd too many learners race condition
-    sleep $(( LOCATION_IN_ARRAY*300 ))
+    sleep $((LOCATION_IN_ARRAY * 300))
 
     status="success"
     /root/installer/install-uipath.sh -i /root/installer/input.json -o /root/installer/output.json -k -j server --accept-license-agreement --skip-pre-reqs || status="failed"
@@ -155,23 +191,27 @@ function main() {
       echo "The RKE2 server failed to start"
       exit 1
     fi
+    if ((SETUPBACKUP == 1)); then
+      /root/installer/install-uipath.sh -i /root/installer/backup.json -o /root/installer/output-backup.json -b -j server --accept-license-agreement
+    fi
   fi
 
   try=0
   maxtry=10
 
-  while [[ $(kubectl get --raw='/readyz') != "ok" ]] && (( try != maxtry )); do
-    try=$((try+1))
+  while [[ $(kubectl get --raw='/readyz') != "ok" ]] && ((try != maxtry)); do
+    try=$((try + 1))
     echo "waiting for kubernetes api server to be ready...${try}/${maxtry}" &&  sleep 10;
   done
 
   try=0
-  while [[ $(kubectl get node "$(hostname)" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') =~ "False" ]] && (( try != maxtry )); do
-    try=$((try+1))
+  while [[ $(kubectl get node "$(hostname)" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') =~ "False" ]] && ((try != maxtry)); do
+    try=$((try + 1))
     echo "waiting for server to be ready...${try}/${maxtry}" &&  sleep 10;
   done
 
   kubectl cluster-info &>/dev/null || (echo "Could not reach the cluster API" && exit 1)
 }
 
+parse_long_args "$@"
 main "$@"
